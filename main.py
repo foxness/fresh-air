60517d6daaa6b5e49bd672d54e0a7afdf27d235a
nocturnal <foxnezz@gmail.com>
2019-05-16 08:56:02 +0300

    added dictionarifying

--- main.py
+++ main.py
@@ -21,13 +21,34 @@ def get_sms(line):
     if not line.startswith('<sms '):
         return None
     
-    line = line[5:-3]
-    return line
+    sms = line[5:-3]
+    return sms
+
+def dictionarify_smses(smses):
+    return [dictionarify(sms) for sms in smses]
+
+def dictionarify(sms):
+    index = 0
+    dictionarified = {}
+    
+    while True:
+        eqindex = sms.find('=', index)
+        fieldname = sms[index:eqindex]
+        quoteindex = sms.find('"', eqindex + 2)
+        fieldvalue = sms[eqindex + 2:quoteindex]
+        dictionarified[fieldname] = fieldvalue
+        index = quoteindex + 2
+
+        if index >= len(sms):
+            break
+    
+    return dictionarified
 
 def main():
     fileraw = get_raw_file()
     smses = get_smses(fileraw)
+    smses = dictionarify_smses(smses)
 
-    print(repr(smses[:3]))
+    print(repr(smses[0]))
 
 main()
